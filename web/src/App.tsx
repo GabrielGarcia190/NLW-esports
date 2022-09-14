1d4e8f4d53c3d9e3374185edac6c2fedea47c0fc
gabriegarcia190 <gokugamer190@gmail.com>
2022-09-14 19:35:07 -0300

    Adiciona segunda aula nlw

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,21 @@
+import './styles/main.css';
+import logoImg from './assets/logo-nlw-esports.svg';
+
 function App() {
-  return <h1>Hello Word NLW</h1>
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center m-20">
+      <img src={logoImg} alt="" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className=" text-transparent bg-nlw-gradient bg-clip-text">duo</span> está aqui
+      </h1>
+      <div className="grid grid-cols-6 gap-6">
+      </div>
+
+    </div>
+  )
 }
 
+
+
 export default App
+
